a5f72a39a27ed6e11cd1d826fc9673e191731c95
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-11-27 12:44:06 -0800

    doing

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * cheack_cycle - checks if a linked contains a cycle
+ * @list: linked to check
+ *
+ * Return: 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
+}
