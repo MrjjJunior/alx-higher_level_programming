43a727c2510a362a9a71cbe73a092fdc0c9990d3
Tshepiso Tlhong <tlhongtshepiso2@gmail.com>
2023-11-28 05:02:48 -0800

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,5 +4,5 @@ while num < 100:
     if num < 10:
         print(f"0{num},", end=' ')
     else:
-        print(f"{num},", end=' ')
+        print(f"{num},", end=' ' if num != 99 else "\n")
     num = num + 1
