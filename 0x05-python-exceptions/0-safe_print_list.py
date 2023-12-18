a354e0e57d28aac76d75b1b49de2d9239128f53f
MrjjJunior <132932795+MrjjJunior@users.noreply.github.com>
2023-12-18 13:44:11 +0200

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,13 +12,13 @@ def safe_print_list(my_list=[], x=0):
 
     """
 
-    point = 0
+    ret = 0
 
     for i in range(x):
         try:
             print("{}".fromat(my_list[i], end="")
-            point = point + 1
+            ret += 1
         except IndexError:
                   break
     print("")
-    return (point)
+    return (ret)
