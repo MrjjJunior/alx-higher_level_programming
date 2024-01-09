1540fb3135cfcdea5123af4c106374b20d8d7ac5
MrjjJunior <132932795+MrjjJunior@users.noreply.github.com>
2024-01-09 14:20:30 +0200

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-''' defines a function that reads text files '''
+"""Defines a text file-reading function."""
+
 
 def read_file(filename=""):
-    """ Prints contents of a UTF8 text file to stdout """
-
-    with open('my_file_0.txt', 'r') as f:
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
