17500a18fb21df7bada305bd2275bd1e89cd74b8
MrjjJunior <132932795+MrjjJunior@users.noreply.github.com>
2024-01-09 14:12:14 +0200

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' define a function that reads files '''
+
+def read_file(filename=""):
+    ''' Prints contents of my_file_0.txt'''
+
+    with open('my_file_0.txt', 'r') as f:
+        rf = f.read()
+        print(rf)
