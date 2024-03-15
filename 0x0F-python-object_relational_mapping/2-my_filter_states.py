04207dadc1d6a97b84978093bdfeafb0e0c4e19b
Tshepiso <tlhongtshepiso2@gmail.com>
2024-03-15 14:18:53 +0200

    fixing checks by fixing up code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-''' lists all states from database '''
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     database  = sys.argv[3]
+    state_searched = sys.argv[4]
 
-    
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=username,
             passwd=password,
             db=database
+            #s_search=state_searched
     )
 
-    
     cursor = db.cursor()
 
-    
-    cursor.execute("SELECT * FROM states")
+    sql_query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
 
-    
+    cursor.execute(sql_query, (state_searched,3-my_safe_filter_states.py))
+
     results = cursor.fetchall()
     for row in results:
         print(row)
