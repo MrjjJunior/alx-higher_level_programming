a4e517bb02c073b6380ff4155390373b7a91636f
root <root@LAPTOP-AF16AO1G.>
2024-03-28 13:53:27 +0200

    Task0 fixing up task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,15 @@
 
 from urllib.request import urlopen
 
-with urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-    utf8 = body.decode('utf-8')
+if __name__ == "__main__":
+
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        utf8 = body.decode('utf-8')
 
 
-print("Body response:")
-print("    - type:", type(body))
-print("    -content", body)
-print("    - utf8 content:", utf8)
+    print("Body response:")
+    print("    - type:", type(body))
+    print("    - content", body)
+    print("    - utf8 content:", utf8)
 
